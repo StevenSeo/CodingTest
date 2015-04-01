aa2bda7c23999f84284bb24eeae2af25dd86f9b6
Chris Moschini <chris@brass9.com>
2015-04-01 11:40:50 -0400

    Standard VS2013 MVC csproj, with ViewStart removed.

---
+++ CodingTest/Migrations/Configuration.cs
@@ -0,0 +1,15 @@
+namespace CodingTest.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CodingTest.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+    }
+}
